1bb287f46c0773bc24d0ecbab12f877255d1bfd1
ART-expert <abdulrafay03@gmail.com>
2021-11-15 10:44:04 -0800

    Add Stateful widget and Randomize image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 void main() => runApp(
       MaterialApp(
@@ -17,7 +18,32 @@ class BallPage extends StatelessWidget {
         title: Text('Ask me Anything'),
         backgroundColor: Colors.blue.shade900,
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+            print("$ballNumber");
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
     );
   }
 }
